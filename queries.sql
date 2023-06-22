068954835ddd38b54bd410d5ee8e1bcbd2ada059
alromannov <135010747+alromannov@users.noreply.github.com>
2023-06-22 22:05:14 +0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,2 +1,29 @@
-INSERT INTO customers (customers_count) VALUES
-	 (19759);
+/*отчет с продавцами у которых наибольшая выручка*/
+with tab as(
+select s.sales_id, s.sales_person_id, CONCAT(e.first_name,' ', e.last_name) as name, (p.price * s.quantity) as income 
+from sales s
+left join employees e 
+on s.sales_person_id = e.employee_id 
+left join products p
+on s.product_id  = p.product_id 
+), 
+sq1 as (
+select name, SUM(income) as income
+from tab 
+group by 1 
+),
+sq2 as (
+select sales_person_id, CONCAT(e.first_name,' ', e.last_name) as name, COUNT(sales_id) as operations 
+from sales s 
+left join employees e 
+on s.sales_person_id = e.employee_id 
+group by 1, 2
+order by 3 desc 
+)
+select sq2.name, operations, income
+from sq2
+inner join sq1
+on sq1.name = sq2.name 
+order by income desc
+limit 10
+
